9599989489d4ff9b150ca4f63f0095d442221a3b
PJAheibam <prosenjitsingha28@gmail.com>
2023-07-29 23:34:22 +0600

    feature categories section added

--- src/components/ui/home/FeatureCategories.js
+++ src/components/ui/home/FeatureCategories.js
@@ -1,24 +1,42 @@
 import React from "react";
+import Image from "next/image";
 
 const categories = [
-  ["cpu", "CPU/Processor"],
-  ["motherboard", "Motherboard"],
-  ["ram", "RAM"],
-  ["psu", "PSU"],
-  ["storage", "Storage"],
-  ["monitor", "Monitor"],
-  ["gpu", "GPU"],
-  ["mouse", "Mouse"],
-  ["keyboard", "Keyboard"],
-  ["others", "Others"],
+  ["cpu.png", "CPU/Processor"],
+  ["motherboard.png", "Motherboard"],
+  ["ram.png", "RAM"],
+  ["psu.png", "PSU"],
+  ["storage.png", "Storage"],
+  ["monitor.png", "Monitor"],
+  ["others.jpg", "Others"],
+  //   ["gpu.png", "GPU"],
+  //   ["mouse.png", "Mouse"],
+  //   ["keyboard.png", "Keyboard"],
 ];
 
 function FeatureCategories() {
   return (
-    <div>
-      <h1 className="my-4 text-3xl font-black text-center">
+    <div className="py-8 my-4">
+      <h1 className="mb-4 text-3xl font-black text-center">
         Feature Categories
       </h1>
+
+      <div className="flex flex-wrap justify-center gap-6">
+        {categories.map(([path, name]) => (
+          <div
+            key={name}
+            className="flex flex-col items-center justify-between gap-1 p-4 bg-slate-100 border border-green-700 min-w-[125px] rounded-md cursor-pointer hover:shadow-lg"
+          >
+            <Image
+              src={`/assets/icons/${path}`}
+              width={60}
+              height={60}
+              alt={name}
+            />
+            <h3 className="text-lg font-medium">{name}</h3>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
--- src/components/ui/home/FeatureProducts.js
+++ src/components/ui/home/FeatureProducts.js
@@ -3,8 +3,10 @@ import React from "react";
 
 function FeatureProducts({ products }) {
   return (
-    <section className="px-8 py-4">
-      <h1 className="my-4 text-3xl font-black text-center">Feature Products</h1>
+    <section className="px-8 my-4">
+      <h1 className="mb-4 text-3xl font-extrabold text-center">
+        Feature Products
+      </h1>
       <div className="grid grid-cols-3 gap-4">
         {products.map((product) => (
           <ProductCard product={product} key={product._id} />
