04f631e671b97aca0be042b0bd16b77b81337ff3
PJAheibam <prosenjitsingha28@gmail.com>
2023-07-29 13:14:38 +0600

    initial project setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,43 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
     },
   },
-  plugins: [],
-}
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#fc2d45",
+
+          secondary: "#325cdb",
+
+          accent: "#9dc62b",
+
+          neutral: "#251f28",
+
+          "base-100": "#fbf9fb",
+
+          info: "#2ab3e5",
+
+          success: "#208373",
+
+          warning: "#f2ae31",
+
+          error: "#f73722",
+        },
+      },
+    ],
+  },
+  plugins: [require("daisyui")],
+};
